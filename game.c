6c42f1098e20ecd90a2f2d1b7f9eb2a3ec57202b
Sebold <sebold.ph@gmail.com>
2020-11-26 12:39:04 -0300

    0.1

--- game.c
+++ game.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <conio.h>
+#include <conio.c>
 
-int main () {
-    printf("Campo Minado \n");
+int i, j, x, y;
+
+int main() {
+    srand(time(NULL));
+    
+    x = 52;
+    y = 10;
+    
+    for (i = 1; i <= 8; i++) {
+        y = y + 1;
+        gotoxy(x, y);
+        printf(" o o o o o o o o \n");
+    }
+    
+    
+    
+    printf("\n");
     system("pause");
     return 0;
 }
